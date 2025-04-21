0e291146460c8b5bdc01f1971c845be61195cffe
RekZNoZ <rafikicroz@hotmail.com>
2025-04-21 20:33:04 +0200

    Create Producto.php

---
+++ Servidor/app/Models/Producto.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio',
+        'stock',
+        'categoria_id',
+        'marca',
+        'modelo',
+        'sku', // Código de referencia del producto (ejemplo “AS‑RIFLE‑001”).
+        'descuento',
+        'fps',
+        'calibre', // Calibre del producto (ejemplo “6 mm”).
+        'capacidad_cargador',
+        'peso',
+        'imagenes', // también JSON si quieres múltiples imágenes
+        'video_demo', // URL del video de demostración
+        'tiempo_envio',
+        'estado_activo',
+    ];
+
+    protected $casts = [
+        'precio' => 'decimal:2',
+        'descuento' => 'decimal:2',
+        'peso' => 'float',
+        'imagenes' => 'array',
+        'estado_activo' => 'boolean',
+    ];
+
+    // Relación con la categoría
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    // Relación con opiniones
+    public function opiniones()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    // Precio final calculado
+    public function getPrecioFinalAttribute()
+    {
+        return $this->precio - $this->descuento;
+    }
+
+}
