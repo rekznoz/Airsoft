526930c20a456637e416fbd5cf6d5ceafca497c4
RekZNoZ <rafikicroz@hotmail.com>
2025-04-21 20:32:58 +0200

    Create ProductoResource.php

---
+++ Servidor/app/Http/Resources/V1/ProductoResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+            'precio' => $this->precio,
+            'descuento' => $this->descuento,
+            'precio_final' => $this->precio_final,
+            'stock' => $this->stock,
+            'categoria' => [
+                'id' => $this->categoria?->id,
+                'nombre' => $this->categoria?->nombre,
+            ],
+            'marca' => $this->marca,
+            'modelo' => $this->modelo,
+            'sku' => $this->sku,
+            'fps' => $this->fps,
+            'calibre' => $this->calibre,
+            'capacidad_cargador' => $this->capacidad_cargador,
+            'peso' => $this->peso,
+            'imagenes' => $this->imagenes,
+            'video_demo' => $this->video_demo,
+            'tiempo_envio' => $this->tiempo_envio,
+            'estado_activo' => $this->estado_activo,
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
