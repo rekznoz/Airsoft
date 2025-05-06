a0f49b9d5bc4ff98857982c2576482f1416054ff
RekZNoZ <rafikicroz@hotmail.com>
2025-05-06 12:29:10 +0200

    Update Comentarios.php

--- Servidor/app/Models/Comentarios.php
+++ Servidor/app/Models/Comentarios.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Comentarios extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_usuario',
+        'id_producto',
+        'comentario',
+        'calificacion',
+    ];
+
+    /**
+     * Relación: Un comentario pertenece a un usuario
+     */
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'id_usuario');
+    }
+
+    /**
+     * Relación: Un comentario pertenece a un producto
+     */
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, 'id_producto');
+    }
+
 }
