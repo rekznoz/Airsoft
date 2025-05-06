673c04ab6c7a88ea6245e314be0adb3c5540a6aa
RekZNoZ <rafikicroz@hotmail.com>
2025-05-06 12:59:13 +0200

    Update ComentariosPolicy.php

--- Servidor/app/Policies/ComentariosPolicy.php
+++ Servidor/app/Policies/ComentariosPolicy.php
@@ -2,7 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\Comentarios;
+use App\Models\Comentario;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -19,7 +19,7 @@ class ComentariosPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Comentarios $comentarios): bool
+    public function view(User $user, Comentario $comentarios): bool
     {
         //
     }
@@ -35,7 +35,7 @@ class ComentariosPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Comentarios $comentarios): bool
+    public function update(User $user, Comentario $comentarios): bool
     {
         //
     }
@@ -43,7 +43,7 @@ class ComentariosPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Comentarios $comentarios): bool
+    public function delete(User $user, Comentario $comentarios): bool
     {
         //
     }
@@ -51,7 +51,7 @@ class ComentariosPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Comentarios $comentarios): bool
+    public function restore(User $user, Comentario $comentarios): bool
     {
         //
     }
@@ -59,7 +59,7 @@ class ComentariosPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Comentarios $comentarios): bool
+    public function forceDelete(User $user, Comentario $comentarios): bool
     {
         //
     }
