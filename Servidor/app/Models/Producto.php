dbbbdbd737ad74f5e63310693e1ccbaa7849cde7
RekZNoZ <rafikicroz@hotmail.com>
2025-05-06 17:13:50 +0200

    Update Producto.php

--- Servidor/app/Models/Producto.php
+++ Servidor/app/Models/Producto.php
@@ -54,4 +54,61 @@ class Producto extends Model
         return $this->precio - $this->descuento;
     }
 
+    public function scopeFilter($query, $filters) {
+
+        if ($filters['search'] ?? false) {
+            $query->where('nombre', 'like', '%' . request('search') . '%')
+                ->orWhere('descripcion', 'like', '%' . request('search') . '%');
+        }
+
+        if ($filters['categoria_id'] ?? false) {
+            $query->where('categoria_id', request('categoria_id'));
+        }
+
+        if ($filters['marca'] ?? false) {
+            $query->where('marca', request('marca'));
+        }
+
+        if ($filters['modelo'] ?? false) {
+            $query->where('modelo', request('modelo'));
+        }
+
+        if ($filters['fps'] ?? false) {
+            $query->where('fps', request('fps'));
+        }
+
+        if ($filters['calibre'] ?? false) {
+            $query->where('calibre', request('calibre'));
+        }
+
+        if ($filters['capacidad_cargador'] ?? false) {
+            $query->where('capacidad_cargador', request('capacidad_cargador'));
+        }
+
+        if ($filters['peso'] ?? false) {
+            $query->where('peso', request('peso'));
+        }
+
+        if ($filters['estado_activo'] ?? false) {
+            $query->where('estado_activo', request('estado_activo'));
+        }
+
+        if ($filters['descuento'] ?? false) {
+            $query->where('descuento', request('descuento'));
+        }
+
+        if ($filters['precio_min'] ?? false) {
+            $query->where('precio', '>=', request('precio_min'));
+        }
+
+        if ($filters['precio_max'] ?? false) {
+            $query->where('precio', '<=', request('precio_max'));
+        }
+
+        if ($filters['stock'] ?? false) {
+            $query->where('stock', request('stock'));
+        }
+
+    }
+
 }
