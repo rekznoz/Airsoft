dcecdadd49f7e50949707701e790b73b0bdcfc97
RekZNoZ <rafikicroz@hotmail.com>
2025-05-06 12:17:27 +0200

    Create CategoriaResource.php

---
+++ Servidor/app/Http/Resources/CategoriaResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoriaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+            'productos' => [
+                'id' => $this->productos->id,
+                'nombre' => $this->productos->nombre,
+                'descripcion' => $this->productos->descripcion,
+                'precio' => $this->productos->precio,
+            ],
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
