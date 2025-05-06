9db282c20b626f9e16dbea3ba30bfb0265c1d1a7
RekZNoZ <rafikicroz@hotmail.com>
2025-05-06 12:20:26 +0200

    Create ComentariosController.php

---
+++ Servidor/app/Http/Controllers/ComentariosController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comentarios;
+use App\Http\Requests\StoreComentariosRequest;
+use App\Http\Requests\UpdateComentariosRequest;
+
+class ComentariosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreComentariosRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comentarios $comentarios)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comentarios $comentarios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateComentariosRequest $request, Comentarios $comentarios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comentarios $comentarios)
+    {
+        //
+    }
+}
