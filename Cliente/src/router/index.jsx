d7c7f7f8a2a0afcdeb35a1bc08f03b45996d146a
RekZNoZ <rafikicroz@hotmail.com>
2025-05-07 19:52:40 +0200

    Create index.jsx

---
+++ Cliente/src/router/index.jsx
@@ -0,0 +1,130 @@
+import {createBrowserRouter} from "react-router-dom";
+import {lazy, Suspense} from "react";
+
+import Loading from "../components/Spinner.jsx";
+
+const Publico = lazy(() => import("../layouts/Publico.jsx"));
+const Inicio = lazy(() => import("../pages/Inicio.jsx"));
+const Error = lazy(() => import("../pages/Error.jsx"));
+
+/**
+ * Router de la aplicación
+ * @type {Router}
+ */
+export const router = createBrowserRouter([
+    {
+        path: "/", // Ruta base de la aplicación
+        element: (
+            <Suspense fallback={<Loading/>}>
+                <Publico/>
+            </Suspense>
+        ),
+        errorElement: (
+            <Suspense fallback={<Loading/>}>
+                <Publico>
+                    <Error/>
+                </Publico>
+            </Suspense>
+        ),
+        children: [
+            {
+                index: true,
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Inicio/>
+                    </Suspense>
+                )
+            },
+            {/*
+                path: "/contacto",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Contacto/>
+                    </Suspense>
+                )
+            },
+            {
+                path: "usuario/:username",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <PerfilUsuario/>
+                    </Suspense>
+                ),
+                loader: obtenerPerfilePorUsername,
+            },
+            {
+                path: "mascotas",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Mascotas/>
+                    </Suspense>
+                ),
+                loader: obtenerAnimales,
+            },
+            {
+                path: "mascota/:id",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Mascota/>
+                    </Suspense>
+                ),
+                loader: obtenerAnimal,
+            },
+            {
+                path: "/tienda",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Tienda/>
+                    </Suspense>
+                ),
+                loader: obtenerProductosRevisados,
+            },
+            {
+                path: "/tienda/:id",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Producto/>
+                    </Suspense>
+                ),
+                loader: obtenerProductoPorId,
+            },
+            {
+                path: "/blog",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Blog/>
+                    </Suspense>
+                ),
+                loader: obtenerPublicacionesRevisadas,
+            },
+            {
+                path: "/blog/:id",
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <Publicacion/>
+                    </Suspense>
+                ),
+                loader: obtenerPublicacionPorId,
+            },
+            {
+                path: '/admin',
+                element: (
+                    <Suspense fallback={<Loading/>}>
+                        <LayoutPrivado/>
+                    </Suspense>
+                ),
+                children: [
+                    {
+                        index: true,
+                        element: (
+                            <Suspense fallback={<Loading/>}>
+                                <Admin/>
+                            </Suspense>
+                        ),
+                        loader: obtenerDatos,
+                    }
+                ]
+            */},
+        ],
+    },
+]);
