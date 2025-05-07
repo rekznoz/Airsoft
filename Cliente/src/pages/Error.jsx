f933b82a689f1249ba45fb288a051866dedb584f
RekZNoZ <rafikicroz@hotmail.com>
2025-05-07 19:52:36 +0200

    Create Error.jsx

---
+++ Cliente/src/pages/Error.jsx
@@ -0,0 +1,10 @@
+
+export default function Error() {
+    return (
+        <div className="container">
+            <h1>Error</h1>
+            <p>Lo sentimos, ha ocurrido un error inesperado.</p>
+            <p>Por favor, inténtalo de nuevo más tarde.</p>
+        </div>
+    )
+}
