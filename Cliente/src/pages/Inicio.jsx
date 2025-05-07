688ac7d405320724cdb4de4e3d6faba11de250b6
RekZNoZ <rafikicroz@hotmail.com>
2025-05-07 19:52:38 +0200

    Create Inicio.jsx

---
+++ Cliente/src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+
+export default function Inicio() {
+    return (
+        <div className="container">
+            <h1>Inicio</h1>
+            <p>Bienvenido a la aplicación de gestión de mascotas.</p>
+            <p>Esta aplicación te permite gestionar tus mascotas de manera eficiente.</p>
+            <p>Explora las diferentes secciones para conocer más sobre nuestras funcionalidades.</p>
+        </div>
+    )
+}
