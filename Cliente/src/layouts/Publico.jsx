2a3a836a31a29c1fddc389a6d0c3a1ffaac96266
RekZNoZ <rafikicroz@hotmail.com>
2025-05-07 19:52:32 +0200

    Create Publico.jsx

---
+++ Cliente/src/layouts/Publico.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Outlet} from "react-router-dom";
+//import Header from "../components/Header.jsx";
+//import Footer from "../components/Footer.jsx";
+//import Login from "../components/Login.jsx";
+
+/**
+ * Componente que define el layout público de la aplicación
+ * @param children
+ * @returns {Element}
+ * @constructor
+ */
+function Publico({ children }) {
+    return (
+        <>
+            {/*<Header/>*/}
+            <main className='main'>
+                {children || <Outlet />}
+            </main>
+            {/*<Footer/>*/}
+            {/*<Login/>*/}
+        </>
+    )
+}
+
+export default Publico
