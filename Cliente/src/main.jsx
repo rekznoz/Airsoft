522f2f498d206cdae4626db04edcbe3bc4dac7cb
RekZNoZ <rafikicroz@hotmail.com>
2025-05-07 19:52:34 +0200

    Update main.jsx

--- Cliente/src/main.jsx
+++ Cliente/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./router/index.jsx";
+
+import "./css/index.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <RouterProvider router={router} />
+  </StrictMode>
 )
