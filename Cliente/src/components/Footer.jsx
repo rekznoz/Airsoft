cdaeac332670b9e2df542c2da9bf704430229070
RekZNoZ <rafikicroz@hotmail.com>
2025-05-10 01:12:59 +0200

    Create Footer.jsx

---
+++ Cliente/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../css/footer.css"
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer-text">© 2025 - Todos los derechos reservados</p>
+        </footer>
+    )
+}
