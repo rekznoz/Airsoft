ffe723324becaf1b3fad82806c5e7a78864cddd3
RekZNoZ <rafikicroz@hotmail.com>
2025-05-10 20:06:12 +0200

    Update Error.jsx

--- Cliente/src/pages/Error.jsx
+++ Cliente/src/pages/Error.jsx
@@ -1,7 +1,7 @@
 
 export default function Error() {
     return (
-        <div className="container">
+        <div className="contenedor">
             <h1>Error</h1>
             <p>Lo sentimos, ha ocurrido un error inesperado.</p>
             <p>Por favor, inténtalo de nuevo más tarde.</p>
