e069a162640bda03b8e78f45ca23a2e3d5fe9f9c
RekZNoZ <rafikicroz@hotmail.com>
2025-05-18 21:47:53 +0200

    Update AuthC.jsx

--- Cliente/src/context/AuthC.jsx
+++ Cliente/src/context/AuthC.jsx
@@ -2,34 +2,17 @@ import {create} from 'zustand';
 import {loguearUsuario} from '../config/AuthService';
 import Swal from "sweetalert2";
 
+const usuarioVacio = {
+    id: 0,
+    name: "",
+    email: "",
+    email_verified_at: "",
+    created_at: "",
+    updated_at: ""
+}
+
 const useAuthStore = create((set) => ({
 
-    user: JSON.parse(localStorage.getItem('user')) || null,
-    isAuthenticated: !!localStorage.getItem('user'),
-
-    login: async (credentials) => {
-        try {
-            const usuario = await loguearUsuario(credentials);
-            set({user: usuario, isAuthenticated: true});
-            localStorage.setItem('user', JSON.stringify(usuario));
-            return true;
-        } catch (error) {
-            Swal.fire({
-                title: 'Error!',
-                text: error.message,
-                icon: 'error',
-                confirmButtonText: 'Aceptar'
-            });
-            return false;
-        }
-    },
-
-    logout: () => {
-        set({user: null, isAuthenticated: false});
-        localStorage.removeItem('user');
-    }
-
 }));
-
 
 export default useAuthStore;
