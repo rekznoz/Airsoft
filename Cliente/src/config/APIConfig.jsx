97f9f5acebca028a81f6264070e750670cd97847
RekZNoZ <rafikicroz@hotmail.com>
2025-05-19 16:50:59 +0200

    Update APIConfig.jsx

--- Cliente/src/config/APIConfig.jsx
+++ Cliente/src/config/APIConfig.jsx
@@ -2,6 +2,9 @@
 
 const apiconfig = {
     productos: import.meta.env.VITE_API_PRODUCTOS_URL,
+    categorias: import.meta.env.VITE_API_CATEGORIAS_URL,
+    pedidos: import.meta.env.VITE_API_PEDIDOS_URL,
+    comentarios: import.meta.env.VITE_API_COMENTARIOS_URL,
     auth: {
         login: import.meta.env.VITE_API_URL_AUTH,
         register: import.meta.env.VITE_API_URL_REGISTER,
