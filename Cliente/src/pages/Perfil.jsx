dcc47fb8c288929bc21ba0a821fcb27a25e9cadb
RekZNoZ <rafikicroz@hotmail.com>
2025-05-19 19:10:54 +0200

    Create Perfil.jsx

---
+++ Cliente/src/pages/Perfil.jsx
@@ -0,0 +1,8 @@
+export default function Perfil() {
+    return (
+        <div className="perfil">
+            <h1>Perfil de Usuario</h1>
+            <p>Bienvenido al perfil de usuario.</p>
+        </div>
+    );
+}
