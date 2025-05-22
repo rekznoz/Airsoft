1d3a87fe89a54c8ea4f013fc587cf387c765a779
RekZNoZ <rafikicroz@hotmail.com>
2025-05-22 18:44:15 +0200

    Update Privado.jsx

--- Cliente/src/layouts/Privado.jsx
+++ Cliente/src/layouts/Privado.jsx
@@ -11,7 +11,7 @@ function Privado({ children }) {
 
     useEffect(() => {
         if (!isLoggedIn) {
-            navigate('/');
+            navigate('/no-perfil');
         }
     }, [isLoggedIn, navigate]);
 
