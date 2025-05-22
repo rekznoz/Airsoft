e2ad6c8095363087ab9e9f9bfbbef95f1ea51d14
RekZNoZ <rafikicroz@hotmail.com>
2025-05-22 16:07:06 +0200

    Update api.php

--- Servidor/routes/api.php
+++ Servidor/routes/api.php
@@ -29,12 +29,14 @@ Route::group([
     'middleware' => ['api', TelescopeMiddleware::class],
     'prefix' => 'auth'
 ], function () {
-    Route::post('login'     , [AuthController::class, 'login'])->name('login');
-    Route::post('logout'    , [AuthController::class, 'logout'])->name('logout');
-    Route::post('refresh'   , [AuthController::class, 'refresh'])->name('refresh');
-    Route::post('me'        , [AuthController::class, 'me'])->name('me');
+    Route::post('login'     , [AuthController::class, 'login'   ])->name('login');
+    Route::post('logout'    , [AuthController::class, 'logout'  ])->name('logout');
+    Route::post('refresh'   , [AuthController::class, 'refresh' ])->name('refresh');
+    Route::post('me'        , [AuthController::class, 'me'      ])->name('me');
     Route::post('register'  , [AuthController::class, 'register'])->name('register');
 });
+
+Route::get('users', [AuthController::class, 'listUsers'])->middleware('auth:api');
 
 Route::apiResource('categorias' , CategoriaController::class)->middleware(['api', TelescopeMiddleware::class]);
 
