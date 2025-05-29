b0f8c04f39fc448e8d1ce43eeccc3f5724307464
RekZNoZ <rafikicroz@hotmail.com>
2025-05-29 16:12:51 +0200

    Update LoginRegistro.jsx

--- Cliente/src/layouts/LoginRegistro.jsx
+++ Cliente/src/layouts/LoginRegistro.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from "react-router-dom";
-import useUserStore from "../context/AuthC.jsx";
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from "react-router-dom"
+import useUserStore from "../context/AuthC.jsx"
 
 /**
  * Layout para rutas de login y registro, para evitar que un usuario logueado acceda a estas rutas
  */
 function LoginRegistro({ children }) {
-    const isLoggedIn = useUserStore(state => state.isLoggedIn);
-    const navigate = useNavigate();
+    const isLoggedIn = useUserStore(state => state.isLoggedIn)
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (isLoggedIn) {
-            navigate('/');
+            navigate('/')
         }
-    }, [isLoggedIn, navigate]);
+    }, [isLoggedIn, navigate])
 
     return (
         <>
             {children || <Outlet />}
         </>
-    );
+    )
 }
 
-export default LoginRegistro;
+export default LoginRegistro
