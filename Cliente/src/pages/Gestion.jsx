dfef9adb83b730b5b53da7994a313c3acb291aa9
RekZNoZ <rafikicroz@hotmail.com>
2025-05-29 18:59:44 +0200

    Update Gestion.jsx

--- Cliente/src/pages/Gestion.jsx
+++ Cliente/src/pages/Gestion.jsx
@@ -1,9 +1,17 @@
+import {useLoaderData} from "react-router-dom";
+
 export default function Gestion() {
+
+    const {productos, pedidos, comentarios, categorias} = useLoaderData();
+
+    console.log(productos)
+    console.log(pedidos)
+    console.log(comentarios)
+    console.log(categorias)
+
     return (
-        <div className="gestion">
-            <h1>Gestión de la aplicación</h1>
-            <p>Esta sección está en desarrollo.</p>
-            <p>Pronto podrás gestionar usuarios, productos y más.</p>
+        <div className="gestion-contenedor">
+            <h2>Gestión de Productos</h2>
         </div>
     )
 }
