b94936a97cdb3764831d96e9ac77829f04708840
RekZNoZ <rafikicroz@hotmail.com>
2025-05-29 16:02:38 +0200

    Create Gestion.jsx

---
+++ Cliente/src/pages/Gestion.jsx
@@ -0,0 +1,9 @@
+export default function Gestion() {
+    return (
+        <div className="gestion">
+            <h1>Gestión de la aplicación</h1>
+            <p>Esta sección está en desarrollo.</p>
+            <p>Pronto podrás gestionar usuarios, productos y más.</p>
+        </div>
+    );
+}
