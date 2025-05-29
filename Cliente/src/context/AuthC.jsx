c89f3abdb84ae2d05daba97b731145defb06ee0e
RekZNoZ <rafikicroz@hotmail.com>
2025-05-29 16:02:32 +0200

    Update AuthC.jsx

--- Cliente/src/context/AuthC.jsx
+++ Cliente/src/context/AuthC.jsx
@@ -16,14 +16,6 @@ const useUserStore = create(persist((set, get) => ({
         isLoggedIn: false,
         access_token: null,
 
-        cart: [
-
-        ],
-
-        // =====================
-        // 👤 Usuario
-        // =====================
-
         login: ({user, access_token}) => {
             if (!access_token) return;
             set({
@@ -38,7 +30,6 @@ const useUserStore = create(persist((set, get) => ({
                 isLoggedIn: false,
                 user: usuarioVacio,
                 access_token: null,
-                cart: []
             });
         },
 
@@ -60,44 +51,6 @@ const useUserStore = create(persist((set, get) => ({
                 }
             }
         },
-
-        // =====================
-        // 🛒 Carrito de compras
-        // =====================
-
-        addToCart: (producto) => {
-
-            producto.precio = parseFloat(producto.precio);
-
-            //console.log(producto)
-
-            const cart = get().cart;
-            const existingItem = cart.find(item => item.id === producto.id);
-
-            if (existingItem) {
-                const updatedCart = cart.map(item =>
-                    item.id === producto.id
-                        ? {...item, cantidad: item.cantidad + 1}
-                        : item
-                );
-                set({cart: updatedCart});
-            } else {
-                set({cart: [...cart, {...producto, cantidad: 1}]});
-            }
-        },
-
-        removeFromCart: (productId) => {
-            const updatedCart = get().cart.filter(item => item.id !== productId);
-            set({cart: updatedCart});
-        },
-
-        clearCart: () => {
-            set({cart: []});
-        },
-
-        get totalItems() {
-            return get().cart.reduce((acc, item) => acc + item.cantidad, 0);
-        }
 
     }),
     {
