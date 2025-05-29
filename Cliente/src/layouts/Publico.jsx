9904989e6d3c263bb42adf1557705e585dacc04e
RekZNoZ <rafikicroz@hotmail.com>
2025-05-29 16:13:09 +0200

    Update Publico.jsx

--- Cliente/src/layouts/Publico.jsx
+++ Cliente/src/layouts/Publico.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Outlet} from "react-router-dom";
-import Header from "../components/Header.jsx";
-import Footer from "../components/Footer.jsx";
-import useUserStore from "../context/AuthC.jsx";
+import {Outlet} from "react-router-dom"
+import Header from "../components/Header.jsx"
+import Footer from "../components/Footer.jsx"
+import useUserStore from "../context/AuthC.jsx"
 
-//import Login from "../components/Login.jsx";
+//import Login from "../components/Login.jsx"
 
 /**
  * Componente que define el layout público de la aplicación
