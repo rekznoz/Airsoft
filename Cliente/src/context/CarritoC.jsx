0de84f4ae9c8852391b7bb2886b7ce0a1e1da8e8
RekZNoZ <rafikicroz@hotmail.com>
2025-05-29 16:12:44 +0200

    Update CarritoC.jsx

--- Cliente/src/context/CarritoC.jsx
+++ Cliente/src/context/CarritoC.jsx
@@ -1,45 +1,45 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 const useCartStore = create(persist(
     (set, get) => ({
         cart: [],
 
         addToCart: (producto) => {
-            producto.precio = parseFloat(producto.precio);
+            producto.precio = parseFloat(producto.precio)
 
-            const cart = get().cart;
-            const existingItem = cart.find(item => item.id === producto.id);
+            const cart = get().cart
+            const existingItem = cart.find(item => item.id === producto.id)
 
             if (existingItem) {
                 const updatedCart = cart.map(item =>
                     item.id === producto.id
                         ? { ...item, cantidad: item.cantidad + 1 }
                         : item
-                );
-                set({ cart: updatedCart });
+                )
+                set({ cart: updatedCart })
             } else {
-                set({ cart: [...cart, { ...producto, cantidad: 1 }] });
+                set({ cart: [...cart, { ...producto, cantidad: 1 }] })
             }
         },
 
         removeFromCart: (productId) => {
-            const updatedCart = get().cart.filter(item => item.id !== productId);
-            set({ cart: updatedCart });
+            const updatedCart = get().cart.filter(item => item.id !== productId)
+            set({ cart: updatedCart })
         },
 
         clearCart: () => {
-            set({ cart: [] });
+            set({ cart: [] })
         },
 
         get totalItems() {
-            return get().cart.reduce((acc, item) => acc + item.cantidad, 0);
+            return get().cart.reduce((acc, item) => acc + item.cantidad, 0)
         }
     }),
     {
         name: 'cartStore',
         getStorage: () => localStorage, // mejor que sessionStorage para carritos
     }
-));
+))
 
-export default useCartStore;
+export default useCartStore
