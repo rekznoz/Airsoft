9a2d4057c95d2f4730c9e86606ca7d025ba799dd
RekZNoZ <rafikicroz@hotmail.com>
2025-06-03 21:00:02 +0200

    Create Pedidos.jsx

---
+++ Cliente/src/components/Gestion/Pedidos.jsx
@@ -0,0 +1,8 @@
+export default function Pedidos({pedidos}) {
+    return (
+        <div className="pedidos-contenedor">
+            <h1>Pedidos</h1>
+            <p>Aquí se mostrarán los pedidos. (Funcionalidad pendiente)</p>
+        </div>
+    )
+}
