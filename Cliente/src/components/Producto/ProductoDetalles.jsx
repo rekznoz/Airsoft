2f89ba64805c9bb7bbfefd8e919c24ec1e248d16
RekZNoZ <rafikicroz@hotmail.com>
2025-06-07 16:10:04 +0200

    Update ProductoDetalles.jsx

--- Cliente/src/components/Producto/ProductoDetalles.jsx
+++ Cliente/src/components/Producto/ProductoDetalles.jsx
@@ -2,17 +2,6 @@ export default function ProductoDetalles({producto}) {
     return (
         <>
             <section className="producto-detalles">
-                <h2>Detalles</h2>
-                <ul>
-                    <li><strong>Precio base:</strong> {producto.precio} €</li>
-                    <li><strong>Descuento:</strong> {producto.descuento} €</li>
-                    <li><strong>Precio final:</strong> {producto.precio_final} €</li>
-                    <li><strong>Stock:</strong> {producto.stock > 0 ? `${producto.stock} unidades` : "Agotado"}</li>
-                    <li><strong>Tiempo de envío:</strong> {producto.tiempo_envio}</li>
-                    <li><strong>Estado:</strong> {producto.estado_activo ? "Activo" : "Inactivo"}</li>
-                </ul>
-            </section>
-            <section className="producto-detalles">
                 <h2>Especificaciones</h2>
                 <ul>
                     <li><strong>Marca:</strong> {producto.marca}</li>
@@ -22,6 +11,8 @@ export default function ProductoDetalles({producto}) {
                     <li><strong>Calibre:</strong> {producto.calibre}</li>
                     <li><strong>Capacidad del cargador:</strong> {producto.capacidad_cargador}</li>
                     <li><strong>Peso:</strong> {producto.peso} kg</li>
+                    <li><strong>Tiempo de envío:</strong> {producto.tiempo_envio}</li>
+                    <li><strong>Stock:</strong> {producto.stock > 0 ? `${producto.stock} unidades` : "Agotado"}</li>
                 </ul>
             </section>
         </>
