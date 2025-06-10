0bba2ec18fa95a7afae3ec3ce47830bb9075acfa
RekZNoZ <rafikicroz@hotmail.com>
2025-06-10 23:21:39 +0200

    Update Producto.jsx

--- Cliente/src/pages/Producto.jsx
+++ Cliente/src/pages/Producto.jsx
@@ -61,7 +61,6 @@ export default function Producto() {
                 </section>
             )}
 
-
             <ComentariosPaginados comentarios={producto.array_comentarios} producto={producto}/>
 
         </div>
