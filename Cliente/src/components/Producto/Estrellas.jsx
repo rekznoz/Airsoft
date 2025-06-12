4b7657e99b348f9766572cef04dcc75d412b1f04
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:35 +0200

    Update Estrellas.jsx

--- Cliente/src/components/Producto/Estrellas.jsx
+++ Cliente/src/components/Producto/Estrellas.jsx
@@ -1,3 +1,21 @@
+/**
+ * Estrellas.jsx
+ * @param calificacion
+ * @param max
+ * @param interactivas
+ * @param valorSeleccionado
+ * @param setValor
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Componente para mostrar estrellas de calificación.
+ * Este componente muestra un número de estrellas basado en la calificación proporcionada.
+ * Si `interactivas` es `true`, permite al usuario seleccionar una calificación haciendo clic en las estrellas.
+ * @param {number} calificacion - Calificación del producto (0-10).
+ * @param {number} [max=5] - Número máximo de estrellas a mostrar (por defecto 5).
+ * @param {boolean} [interactivas=false] - Si es `true`, las estrellas son interactivas y permiten seleccionar una calificación.
+ * @param {number} [valorSeleccionado=0] - Valor de calificación seleccionado por el usuario (0-5).
+ * @param {function} setValor - Función para actualizar el valor de calificación seleccionado.
+ */
 export default function Estrellas({
                                       calificacion = 0,
                                       max = 5,
