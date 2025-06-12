570872fdbd038151946e902e0a0ee0776c8972a4
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:32 +0200

    Update FormularioNuevoComentario.jsx

--- Cliente/src/components/Producto/Comentarios/FormularioNuevoComentario.jsx
+++ Cliente/src/components/Producto/Comentarios/FormularioNuevoComentario.jsx
@@ -1,5 +1,23 @@
 import Estrellas from "../Estrellas.jsx"
 
+/**
+ * Componente para crear un nuevo comentario de producto.
+ * @param onSubmit
+ * @param comentario
+ * @param setComentario
+ * @param calificacion
+ * @param setCalificacion
+ * @param enviando
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Este componente permite al usuario escribir un nuevo comentario y calificación para un producto.
+ * @param {function} onSubmit - Función que se ejecuta al enviar el formulario.
+ * @param {string} comentario - Texto del comentario.
+ * @param {function} setComentario - Función para actualizar el texto del comentario.
+ * @param {number} calificacion - Calificación del comentario.
+ * @param {function} setCalificacion - Función para actualizar la calificación del comentario.
+ * @param {boolean} enviando - Indica si el formulario está siendo enviado.
+ */
 export default function FormularioNuevoComentario({ onSubmit, comentario, setComentario, calificacion, setCalificacion, enviando }) {
     return (
         <form onSubmit={onSubmit} className="formulario-comentario">
