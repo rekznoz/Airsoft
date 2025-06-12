4aa46a1f1b904312b52295e45a6ca324f72247f1
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:17 +0200

    Update Paginacion.jsx

--- Cliente/src/components/Gestion/Paginacion.jsx
+++ Cliente/src/components/Gestion/Paginacion.jsx
@@ -1,6 +1,18 @@
 import {useState, useEffect} from "react"
 import "../../css/paginacion.css"
 
+/**
+ * Componente de paginación.
+ * @param totalItems
+ * @param itemsPerPage
+ * @param onPageChange
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Este componente permite paginar una lista de elementos, mostrando botones para navegar entre páginas.
+ * @param {number} totalItems - Total de elementos a paginar.
+ * @param {number} [itemsPerPage=6] - Cantidad de elementos por página (por defecto 6).
+ * @param {function} onPageChange - Función que se llama al cambiar de página, recibe un objeto con el rango de elementos y la página actual.
+ */
 export default function Paginacion({totalItems, itemsPerPage = 6, onPageChange}) {
 
     const totalPaginas = Math.ceil(totalItems / itemsPerPage)
