d8753fc484b4732aaeb7ced2a653bc1df65226ba
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 14:12:04 +0200

    Update Pedido.jsx

--- Cliente/src/layouts/Pedido.jsx
+++ Cliente/src/layouts/Pedido.jsx
@@ -5,8 +5,6 @@ import PedidosService from "../services/PedidosService.jsx"
 
 /**
  * Componente de diseño para pedidos.
- * @returns {Element}
- * @constructor
  * @description Este componente se encarga de cargar un pedido específico basado en su ID y verificar los permisos del usuario.
  * Si el usuario no es administrador ni el dueño del pedido, se redirige a una página de no autorizado.
  * Si el pedido no se encuentra, se redirige a una página de no encontrado.
