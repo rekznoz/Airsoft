f4dbe94fcfc1bf417f5c372f1d6653189e208bce
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:37 +0200

    Update GaleriaImagenes.jsx

--- Cliente/src/components/Producto/GaleriaImagenes.jsx
+++ Cliente/src/components/Producto/GaleriaImagenes.jsx
@@ -1,5 +1,15 @@
 import {corregirUrlImagen} from "../../hooks/corregirUrlImagen.jsx"
 
+/**
+ * Componente para mostrar una galería de imágenes de un producto.
+ * @param imagenes
+ * @param setImagenGrande
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Este componente muestra una galería de imágenes del producto y permite seleccionar una imagen para verla en grande.
+ * @param {Array} imagenes - Lista de URLs de las imágenes del producto.
+ * @param {function} setImagenGrande - Función para establecer la imagen grande seleccionada.
+ */
 export default function GaleriaImagenes({imagenes, setImagenGrande}) {
     return (
         <section className="producto-imagenes">
