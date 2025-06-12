b03daadddf30ef909aabddd69e4e6aa4f332d823
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:48 +0200

    Update UsuarioStore.jsx

--- Cliente/src/context/UsuarioStore.jsx
+++ Cliente/src/context/UsuarioStore.jsx
@@ -10,6 +10,19 @@ const usuarioVacio = {
     updated_at: ""
 }
 
+/**
+ * @typedef {import('zustand').UseBoundStore} UseBoundStore
+ * @type {UseBoundStore<Mutate<StoreApi<{user: {id: number, name: string, email: string, email_verified_at: string, created_at: string, updated_at: string}, logueado: boolean, access_token: null, login: function({user: *, access_token: *}): void, logout: function(): void, checkTokenValidity: function(): void}>, [["zustand/persist", {user: {id: number, name: string, email: string, email_verified_at: string, created_at: string, updated_at: string}, logueado: boolean, access_token: null, login: function({user: *, access_token: *}): void, logout: function(): void, checkTokenValidity: function(): void}]]>>}
+ * @description
+ * Store para gestionar el estado del usuario en la aplicación.
+ * Permite manejar el inicio de sesión, cierre de sesión y la validez del token de acceso.
+ * @property {Object} user - Información del usuario autenticado.
+ * @property {boolean} logueado - Indica si el usuario está autenticado.
+ * @property {string|null} access_token - Token de acceso del usuario.
+ * @property {function} login - Función para iniciar sesión del usuario.
+ * @property {function} logout - Función para cerrar sesión del usuario.
+ * @property {function} checkTokenValidity - Función para verificar la validez del token de acceso.
+ * */
 const usuarioStore = create(persist((set, get) => ({
 
         user: usuarioVacio,
