0c305b955d66359724453772f55b4d41b9fdee7e
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:09 +0200

    Update Footer.jsx

--- Cliente/src/components/Footer.jsx
+++ Cliente/src/components/Footer.jsx
@@ -1,5 +1,11 @@
 import "../css/footer.css"
 
+/**
+ * Componente Footer
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Este componente muestra el pie de página de la aplicación, incluyendo derechos de autor.
+ */
 export default function Footer() {
     return (
         <footer className="footer">
