28bcaec20ffdac38b082c89d4367bce132b2d0bc
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:30 +0200

    Update ComentarioItem.jsx

--- Cliente/src/components/Producto/Comentarios/ComentarioItem.jsx
+++ Cliente/src/components/Producto/Comentarios/ComentarioItem.jsx
@@ -1,6 +1,32 @@
 import Estrellas from "../Estrellas.jsx"
 import {Link} from "react-router-dom"
 
+/**
+ * Componente para mostrar un comentario de producto.
+ * @param comentario
+ * @param esUsuarioActual
+ * @param enEdicion
+ * @param manejarEditar
+ * @param manejarGuardarEdicion
+ * @param cancelarEdicion
+ * @param comentarioEditado
+ * @param setComentarioEditado
+ * @param calificacionEditada
+ * @param setCalificacionEditada
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Este componente muestra un comentario de un producto, permitiendo al usuario editarlo si es el autor del comentario.
+ * @param {Object} comentario - Objeto que contiene los detalles del comentario.
+ * @param {boolean} esUsuarioActual - Indica si el comentario pertenece al usuario actual.
+ * @param {boolean} enEdicion - Indica si el comentario está en modo edición.
+ * @param {function} manejarEditar - Función para iniciar la edición del comentario.
+ * @param {function} manejarGuardarEdicion - Función para guardar los cambios del comentario editado.
+ * @param {function} cancelarEdicion - Función para cancelar la edición del comentario.
+ * @param {string} comentarioEditado - Texto del comentario editado.
+ * @param {function} setComentarioEditado - Función para actualizar el texto del comentario editado.
+ * @param {number} calificacionEditada - Calificación editada del comentario.
+ * @param {function} setCalificacionEditada - Función para actualizar la calificación editada.
+ */
 export default function ComentarioItem({ comentario, esUsuarioActual, enEdicion, manejarEditar, manejarGuardarEdicion, cancelarEdicion, comentarioEditado, setComentarioEditado, calificacionEditada, setCalificacionEditada }) {
     return (
         <li key={comentario.id}>
