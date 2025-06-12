73d26fdccee69134da49f00c96c7972458695c35
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 02:17:24 +0200

    Update corregirUrlImagen.jsx

--- Cliente/src/hooks/corregirUrlImagen.jsx
+++ Cliente/src/hooks/corregirUrlImagen.jsx
@@ -1,10 +1,10 @@
 export function corregirUrlImagen(url) {
     try {
-        const urlObj = new URL(url);
-        urlObj.port = '8443';
-        urlObj.protocol = 'https:';
-        return urlObj.toString();
+        const urlObj = new URL(url)
+        urlObj.port = '8443'
+        urlObj.protocol = 'https:'
+        return urlObj.toString()
     } catch (e) {
-        return url;
+        return url
     }
 }
