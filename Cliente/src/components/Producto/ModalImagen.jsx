55fa0f434a07635aac60ac0de77b401d12113c5c
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:39 +0200

    Update ModalImagen.jsx

--- Cliente/src/components/Producto/ModalImagen.jsx
+++ Cliente/src/components/Producto/ModalImagen.jsx
@@ -1,3 +1,14 @@
+/**
+ * ModalImagen.jsx
+ * @param imagenUrl
+ * @param onClose
+ * @returns {JSX.Element}
+ * @constructor
+ * @description Componente para mostrar una imagen ampliada en un modal.
+ * Este componente muestra una imagen en un modal y permite cerrarlo al hacer clic en la imagen o en el botón de cerrar.
+ * @param {string} imagenUrl - URL de la imagen a mostrar.
+ * @param {function} onClose - Función que se llama al cerrar el modal.
+ */
 export default function ModalImagen({imagenUrl, onClose}) {
     return (
         <div className="modal-imagen" onClick={onClose}>
