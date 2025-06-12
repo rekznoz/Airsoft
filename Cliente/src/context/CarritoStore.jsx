912f41e876cf7d520cdd101cc64bf8185e2a9961
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:44 +0200

    Update CarritoStore.jsx

--- Cliente/src/context/CarritoStore.jsx
+++ Cliente/src/context/CarritoStore.jsx
@@ -1,6 +1,20 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import {create} from 'zustand'
+import {persist} from 'zustand/middleware'
 
+/**
+ * @typedef {import('zustand').StoreApi} StoreApi
+ * @type {UseBoundStore<Mutate<StoreApi<{informacionCarrito: [], addToCart: function(*): void, restarFromCart: function(*): void, removeFromCart: function(*): void, clearCart: function(): void, readonly totalItems: *}>, [["zustand/persist", {informacionCarrito: [], addToCart: function(*): void, restarFromCart: function(*): void, removeFromCart: function(*): void, clearCart: function(): void, readonly totalItems: *}]]>>}
+ * @description
+ * Store para manejar el carrito de compras utilizando Zustand y persistencia en localStorage.
+ * Este store permite agregar, restar y eliminar productos del carrito, así como limpiar el carrito completo.
+ * Además, calcula el total de artículos en el carrito.
+ * @property {Array} informacionCarrito - Lista de productos en el carrito.
+ * @property {function} addToCart - Función para agregar un producto al carrito.
+ * @property {function} restarFromCart - Función para restar un producto del carrito.
+ * @property {function} removeFromCart - Función para eliminar un producto del carrito.
+ * @property {function} clearCart - Función para limpiar el carrito completo.
+ * @property {number} totalItems - Número total de artículos en el carrito.
+ */
 const carritoStore = create(persist(
     (set, get) => ({
         informacionCarrito: [],
@@ -14,12 +28,12 @@ const carritoStore = create(persist(
             if (existingItem) {
                 const updatedCart = cart.map(item =>
                     item.id === producto.id
-                        ? { ...item, cantidad: item.cantidad + 1 }
+                        ? {...item, cantidad: item.cantidad + 1}
                         : item
                 )
-                set({ informacionCarrito: updatedCart })
+                set({informacionCarrito: updatedCart})
             } else {
-                set({ informacionCarrito: [...cart, { ...producto, cantidad: 1 }] })
+                set({informacionCarrito: [...cart, {...producto, cantidad: 1}]})
             }
         },
 
@@ -31,25 +45,25 @@ const carritoStore = create(persist(
                 if (existingItem.cantidad > 1) {
                     const updatedCart = cart.map(item =>
                         item.id === producto.id
-                            ? { ...item, cantidad: item.cantidad - 1 }
+                            ? {...item, cantidad: item.cantidad - 1}
                             : item
                     )
-                    set({ informacionCarrito: updatedCart })
+                    set({informacionCarrito: updatedCart})
                 } else {
                     // Si la cantidad es 1, lo eliminamos del carrito
                     const updatedCart = cart.filter(item => item.id !== producto.id)
-                    set({ informacionCarrito: updatedCart })
+                    set({informacionCarrito: updatedCart})
                 }
             }
         },
 
         removeFromCart: (productId) => {
             const updatedCart = get().informacionCarrito.filter(item => item.id !== productId)
-            set({ informacionCarrito: updatedCart })
+            set({informacionCarrito: updatedCart})
         },
 
         clearCart: () => {
-            set({ informacionCarrito: [] })
+            set({informacionCarrito: []})
         },
 
         get totalItems() {
