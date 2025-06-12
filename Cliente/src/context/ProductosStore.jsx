92f11a587c096024484380730ce56ead9e5bd938
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:46 +0200

    Update ProductosStore.jsx

--- Cliente/src/context/ProductosStore.jsx
+++ Cliente/src/context/ProductosStore.jsx
@@ -2,6 +2,20 @@ import { create } from 'zustand'
 import Swal from 'sweetalert2'
 import productoService from '../services/ProductoService.jsx'
 
+/**
+ * Store para gestionar productos en la aplicación.
+ * @type {UseBoundStore<Mutate<StoreApi<{productos: [], cargando: boolean, error: null, productosCargados: boolean, cargarProductos: function(): Promise<void>, resetProductos: function(): void}>, []>>}
+ * @description
+ * Este store utiliza Zustand para manejar el estado de los productos en la aplicación.
+ * Permite cargar productos desde un servicio, manejar el estado de carga y errores,
+ * y resetear la lista de productos cargados.
+ * @property {Array} productos - Lista de productos cargados.
+ * @property {boolean} cargando - Indica si los productos están siendo cargados.
+ * @property {null|string} error - Mensaje de error si ocurre algún problema al cargar los productos.
+ * @property {boolean} productosCargados - Indica si los productos ya han sido cargados.
+ * @property {function} cargarProductos - Función para cargar los productos desde el servicio.
+ * @property {function} resetProductos - Función para resetear la lista de productos cargados.
+ */
 const productosStore = create((set, get) => ({
 
     productos: [],
