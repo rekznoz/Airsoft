4dd50d7d06a8ef1482745537df0083c04dfaf2dd
RekZNoZ <rafikicroz@hotmail.com>
2025-06-12 13:49:49 +0200

    Update corregirUrlImagen.jsx

--- Cliente/src/hooks/corregirUrlImagen.jsx
+++ Cliente/src/hooks/corregirUrlImagen.jsx
@@ -1,3 +1,10 @@
+/**
+ * Corrección de URL de imagen
+ * @param url
+ * @returns {*|string}
+ * @description Esta función toma una URL de imagen y la corrige para que use el protocolo HTTPS y el puerto 8443.
+ * @param {string} url - La URL de la imagen a corregir.
+ */
 export function corregirUrlImagen(url) {
     try {
         const urlObj = new URL(url)
